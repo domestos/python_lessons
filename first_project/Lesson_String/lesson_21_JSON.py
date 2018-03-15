3f5654be52892e4e75fa0393bebe6465333e5ed0
Valera Pelenskyi <elbarto2006@ukr.net>
2018-03-15 18:12:34 +0200

    Json

--- first_project/Lesson_String/lesson_21_JSON.py
+++ first_project/Lesson_String/lesson_21_JSON.py
@@ -1,6 +1,36 @@
+import json
+filename = "user_settings.txt"
+myfile = open(filename, mode='w', encoding='utf8')
+
 
 player1 = {
     "PelayerName": "Donald Trump",
     "Score": 354,
     "awards": ["OR", "NV", "NY"]
 }
+
+player2 = {
+    "PelayerName": "valera pelenskyi",
+    "Score": 354,
+    "awards": ["OR", "NV", "LV"]
+
+}
+
+
+myplayers = []
+myplayers.append(player1)
+myplayers.append(player2)
+
+#--------------- SAVE JSON --------------------
+json.dump(myplayers, myfile)
+myfile.close()
+
+
+#----------------LOAD JSON --------------------
+myfile = open(filename, mode='r', encoding="utf8")
+json_data = json.load(myfile)
+
+for player in json_data:
+    print("Playr Name is "+ str(player['PelayerName']))
+    print("Score: "+str(player['Score']))
+    print("awards:"+ str(player['awards']))
